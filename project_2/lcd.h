cfed72aec3b1c92fa6caa13a17262233a3432dc3
Joel Akiyoshi <akiyoshj@uci.edu>
2024-05-02 14:25:46 -0700

    added underscore to name

---
+++ project_2/lcd.h
@@ -0,0 +1,23 @@
+/*
+ * CFile1.c
+ *
+ * Created: 4/26/2024 12:25:14 PM
+ *  Author: Chan Young Ji & Joel Akiyoshi
+ */ 
+
+#ifndef _LCD_H
+#define _LCD_H
+
+void lcd_init(void);
+
+void lcd_clr(void);
+
+void lcd_pos(unsigned char r, unsigned char c);
+
+void lcd_put(char c);
+
+void lcd_puts1(const char *s);
+
+void lcd_puts2(const char *s);
+
+#endif /* _LCD_H */
